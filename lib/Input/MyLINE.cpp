5ad55c2b694a32abbc5f1aea4d559be7be8e1f5f
okomeongiri <kusashiod@gmail.com>
2025-04-29 21:49:26 +0900

    //あたおか

--- lib/Input/MyLINE.cpp
+++ lib/Input/MyLINE.cpp
@@ -1,7 +1,17 @@
 #include <MyLINE.h>
 
 void MyLINE::MyLINE_Setup() {
-    for (int i = 0; i < NUMLINES; i++) {
-        pinMode(LINE_PIN[i], INPUT); // Set each line sensor pin as input
-    }
-}
+    // for (int i = 0; i < NUMLINES; i++) {
+    //     pinMode(LINE_PIN[i], INPUT); // Set each line sensor pin as input
+    // }
+
+};
+
+int MyLINE::Read_LINE()
+{
+//こゆこと?
+};
+
+int MyLINE::Line_Read()
+{
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <stdio.h>
 #include <MyPIXEL.h>
 #include "Output.h"
 
