5c5858b97bb47231d1eeb78887cee9f507f7c1bd
Fox <fox.vega.soft@gmail.com>
2025-05-02 16:20:10 +0900

    ブランチ統合用

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,10 @@
 #include <Arduino.h>
-#include <stdio.h>
-#include <MyPIXEL.h>
 #include "Output.h"
 
 
 void setup() {
     Serial.begin(9600);
-    mypixel.MyPIXEL_Setup();
 }
     
 void loop() {
-    mybuzzer.MyBUZZER_Preset(1); // Start the buzzer with a note and duration
-    mypixel.MyPIXEL_Multi(1, 5, 255, 0, 0);
 }
