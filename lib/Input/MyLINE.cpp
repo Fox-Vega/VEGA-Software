a4b331a1e3be527df22f6cc242db623e9170d4ba
okomeongiri <kusashiod@gmail.com>
2025-05-02 16:11:09 +0900

    //ぷっしゅ

--- lib/Input/MyLINE.cpp
+++ lib/Input/MyLINE.cpp
@@ -1,15 +1,20 @@
 #include <MyLINE.h>
+#include <Arduino.h>
+
+#define true 1
+#define false 0
+
+int line_deg_list[24]={0,15,30,45,60,75,90,105,120,135,150,165,180,195,210,225,240,255,270,285,300,315,330,345};
 
 void MyLINE::MyLINE_Setup() {
     // for (int i = 0; i < NUMLINES; i++) {
     //     pinMode(LINE_PIN[i], INPUT); // Set each line sensor pin as input
     // }
-
 };
 
 int MyLINE::Read_LINE()
 {
-//こゆこと?
+    
 };
 
 int MyLINE::Line_Read()
