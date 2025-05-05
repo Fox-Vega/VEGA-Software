85f05e963e0b7b2c3f6df9bcf547eddbaf034e99
Fox <fox.vega.soft@gmail.com>
2025-05-05 21:42:03 +0900

    ジャイロ自己位置推定を実装

---
+++ lib/Input/Gyro.cpp
@@ -0,0 +1,66 @@
+#include <Gyro.h>
+
+void Gyro::GyroSetup() {
+    Serial.begin(9600);
+    bno.setExtCrystalUse(true);
+    bno.setMode(OPERATION_MODE_IMUPLUS);
+    delay(25);
+    lastUpdateTime = millis();
+}
+
+void Gyro::GyroRead() {
+    sensors_event_t event;
+    bno.getEvent(&event);
+
+    heading = event.orientation.x;
+    roll    = event.orientation.y;
+    pitch   = event.orientation.z;
+}
+
+void Gyro::GetPosition() {
+    currentTime = millis();
+    dt = (currentTime - lastUpdateTime) / 1000.0;
+    lastUpdateTime = currentTime;
+    sensors_event_t accelEvent, gyroEvent;
+    bno.getEvent(&accelEvent, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    bno.getEvent(&gyroEvent, Adafruit_BNO055::VECTOR_GYROSCOPE);
+    accelX = accelEvent.acceleration.x;
+    accelY = accelEvent.acceleration.y;
+    gyroZ = gyroEvent.gyro.z;
+    theta += gyroZ * dt; //角度更新
+
+    //回転座標に変換してコート視点の座標に調整
+    accelX_rot = accelX * cos(theta) - accelY * sin(theta);
+    accelY_rot = accelX * sin(theta) + accelY * cos(theta);
+
+    TweakKalman(); // カルマンフィルタを調整
+
+    //UKF予測
+    velX += accelX_rot * dt + processNoise;
+    velY += accelY_rot * dt + processNoise;
+    posX += velX * dt;
+    posY += velY * dt;
+
+    //EKF校正
+    posX += measurementNoise;
+    posY += measurementNoise;
+}
+
+void Gyro::TweakKalman() {
+    accelMagnitude = sqrt(accelX * accelX + accelY * accelY);
+    if (accelMagnitude > 2.0) {  
+        processNoise = 0.05;
+        measurementNoise = 0.2;
+    } else {
+        processNoise = 0.01;
+        measurementNoise = 0.1;
+    }
+}
+
+void Gyro::GyroReset() {
+    bno.setMode(OPERATION_MODE_CONFIG);
+    delay(25);
+    bno.setMode(OPERATION_MODE_IMUPLUS);
+    delay(25);
+}
+
--- lib/Input/Gyro.h
+++ lib/Input/Gyro.h
@@ -1,1 +1,35 @@
 #include <Arduino.h> 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+class Gyro {
+    public:
+        void GyroSetup();
+        void GyroRead();
+        void GetPosition();
+        void TweakKalman();
+        void GyroReset();
+        float Accelcm_X;
+        float Accelcm_Y;
+        float heading;
+        float roll;
+        float pitch;
+        //自己位置推定用
+        float posX = 0.0, posY = 0.0;
+        float velX = 0.0, velY = 0.0;
+        float theta = 0.0;
+        float accelX = 0.0, accelY = 0.0; 
+        float gyroZ = 0.0;
+        float processNoise = 0.01; 
+        float measurementNoise = 0.1;
+        unsigned long lastUpdateTime = 0;
+        unsigned long currentTime;
+        float dt;
+        float accelX_rot;
+        float accelY_rot;
+        float accelMagnitude;
+    private:
+        Adafruit_BNO055 bno = Adafruit_BNO055(55);
+};
