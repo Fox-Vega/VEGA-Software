7016cb4518078521dbdcfc4278d8c91cad2ab112
Fox <fox.vega.soft@gmail.com>
2025-05-07 17:11:17 +0900

    モーター制御プログラム追加 バグチェック未完了（とりあえずコミット）

--- lib/Output/motor.cpp
+++ lib/Output/motor.cpp
@@ -1,1 +1,25 @@
 #include <motor.h>
+#include <API.h>
+
+void MOTOR::move(int Azimuth, int Distance, int Power_) {
+    for (int i = 0; i < 4; i++) {
+        AzimuthM = Azimuth - motor_degrees[i];
+        myvector.get_cordinate(AzimuthM, Distance);
+        motor_x[i] = myvector.get_x();
+        motor_y[i] = myvector.get_y();
+        myvector.get_cordinate(AzimuthM, Power_);
+        Power = myvector.get_x();
+        if (Power < 0) {
+            PoMi = false;
+        } else {
+            PoMi = true;
+        }
+        if (PoMi == true) {
+            analogWrite(motor_PIN1[i], Power);
+            analogWrite(motor_PIN2[i], 0);
+        } else {
+            analogWrite(motor_PIN1[i], 0);
+            analogWrite(motor_PIN2[i], Power);
+        }
+    }
+}
--- lib/Output/motor.h
+++ lib/Output/motor.h
@@ -1,6 +1,21 @@
 #include <Arduino.h>
 
-class motor {
+class MOTOR {
     public:
+        void move(int Azimuth, int Distance, int Power_);
+        void turn(int Power_);//マイナスの値を入れても大丈夫
+        void brake();
+
     private:
+        //調整用
+        short speed = 100;
+
+        int Power;//モーター用出力
+        bool PoMi;//positive/minusの略
+        short motor_PIN1[4] = {8, 7, 5, 3};
+        short motor_PIN2[4] = {9, 6, 4, 2};
+        int motor_x[4];
+        int motor_y[4];
+        short AzimuthM;//モーターから見た方位角（一時保存用）
+        short motor_degrees[4] = {45, 135, 225, 315};
 };
