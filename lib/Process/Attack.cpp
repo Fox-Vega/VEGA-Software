97a3dba55e4e0f2d3407cd79cd5c6f029b51e831
Fox <Fox.Vega.Soft@gmail.com>
2025-05-09 10:58:47 +0900

    Update Attack.cpp

--- lib/Process/Attack.cpp
+++ lib/Process/Attack.cpp
@@ -1,11 +1,16 @@
 #include <Attack.h>
 
 void Attack::attack() {
-   if (line.read() == true) {
+    if (line.read() == true) {
        
-   } else {
-       ball.read();
-       if (ball.get_direction() < 10 || ball.get_direction() > 350) {
-       }
-   }
+    } else {
+        ball.read();
+        if (ball.get_direction() < 10 || ball.get_direction() > 350) {
+            attack.b_p1();
+        } else if (ball.get_distance > wrap_rad) {
+            attack.b_p2();
+        } else {
+            attack.b_p3();
+        }
+    }
 }
