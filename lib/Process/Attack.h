de6cd962fe0886ef8f303ca3be1f3721f876f1ed
Fox <Fox.Vega.Soft@gmail.com>
2025-05-09 11:20:31 +0900

    Update Attack.h

--- lib/Process/Attack.h
+++ lib/Process/Attack.h
@@ -3,8 +3,7 @@
 
 class Attack {
     public:
-        void Attack_Setup();
-        void Attack_BallApproach(int BallDirection, int BallDistance);
+        void attack();
     private:
         // Adjustments
         const int BallApproach_radius = 100;
