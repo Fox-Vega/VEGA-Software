8a244bd27b9cc45d06acaa3edf6e58ed51fbb5d9
Fox <Fox.Vega.Soft@gmail.com>
2025-05-09 11:25:32 +0900

    Update Attack.h

--- lib/Process/Attack.h
+++ lib/Process/Attack.h
@@ -4,7 +4,10 @@
 class Attack {
     public:
         void attack();
+        void b_p1();//ボールの回り込みパターン１
+        void b_p2();//ボールの回り込みパターン２
+        void b_p3();//ボールの回り込みパターン３
     private:
         // Adjustments
-        const int BallApproach_radius = 100;
+        const int BallApproach_radius = 100;//回り込み半径
 };
