5a787d6f9a23198f5c928d08524e73bfbb40ec9f
Fox <Fox.Vega.Soft@gmail.com>
2025-05-09 09:24:43 +0900

    Update MyVECTOR.cpp

--- lib/AIP/MyVECTOR.cpp
+++ lib/AIP/MyVECTOR.cpp
@@ -7,13 +7,13 @@ void MyVECTOR::get_cordinate(float Azimuth, float Magnitude) {
     } else if (Theta > 180) {
         Theta -= 360;
     }
-    x = cos(radians(Theta)) * Magnitude;
-    y = sin(radians(Theta)) * Magnitude;
+    x = float(cos(radians(Theta)) * Magnitude);
+    y = float(sin(radians(Theta)) * Magnitude);
 }
 
 void MyVECTOR::get_ppcordinate(Pl_X, Pl_Y, Po_X, Po_Y) {
-    Ps_X = abs(Po_X - Pl_X);
-    Ps_Y = abs(Po_Y - Po_Y);
+    Ps_X = float(abs(Po_X - Pl_X));
+    Ps_Y = float(abs(Po_Y - Po_Y));
     if (Po_X < Pl_X) {
         Ps_X = Ps_X - (Ps_X * 2);
     } else if (Po_Y < Pl_Y) {
@@ -23,7 +23,7 @@ void MyVECTOR::get_ppcordinate(Pl_X, Pl_Y, Po_X, Po_Y) {
 
 int MyVECTOR::get_azimuth(float x, float y) {
     Theta = atan2(y, x);
-    Azimuth = 90 - degrees(Theta);
+    Azimuth = float(90 - degrees(Theta));
     if (Azimuth < 0) { 
         Azimuth += 360;
     } else if (Azimuth > 360) {
@@ -33,7 +33,7 @@ int MyVECTOR::get_azimuth(float x, float y) {
 }
 
 int MyVECTOR::get_magnitude(float x, float y) {
-    return sqrt(x * x + y * y);
+    return float(sqrt(x * x + y * y));
 }
 
 int MyVECTOR::get_x() {
