60bc1b980b9d83588b3e129596fe85562fdb0af9
Fox <Fox.Vega.Soft@gmail.com>
2025-05-09 11:20:45 +0900

    Update Attack.cpp

--- lib/Process/Attack.cpp
+++ lib/Process/Attack.cpp
@@ -8,7 +8,7 @@ void Attack::attack() {
         ball_dir = ball.get_direction();
         if (ball_dir < 10 || ball_dir > 350) {
             attack.b_p1();
-        } else if (ball.get_distance > wrap_rad) {
+        } else if (ball.get_distance > BallApproach_radius) {
             attack.b_p2();
         } else {
             attack.b_p3();
