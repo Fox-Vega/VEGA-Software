375a35affd88ffda1272a939f08f453d2e9b27c5
Fox <Fox.Vega.Soft@gmail.com>
2025-05-12 14:36:24 +0900

    Update Line.cpp

--- lib/Input/Line.cpp
+++ lib/Input/Line.cpp
@@ -17,13 +17,6 @@ neopixel 24個円型
 // TODO
 // twitter（旧X）の垢復活させる
 //距離を返す
-// linedist完成させるlinedist完成させるlinedist完成させるlinedist完成させるlinedist完成させるlinedist完成させるlinedist完成させる
-// 
-// 
-// 
-// 
-// 
-
 
 //伝言
 //りくはTODO書くな　邪魔だし紛らわしい　
@@ -38,74 +31,79 @@ void LINE::setup() {
     pinMode(readPin3, INPUT);
 }
 
-int LINE::read(){ //読み取りを24かいを三回繰り返して当たっていたら配列に１足して　２以上でboolをtrue
-    for (int i = 0; i < NUMLines; i++) {
-        line_values[i] = {0};
-    }
-    progress = 0;
-    for(int i=0; i<3; i++){
-        for(int i = 0; i < 8; i++){//i8
-            digitalWrite(selectA, BinaryNum[i][0]);
-            digitalWrite(selectB, BinaryNum[i][1]);
-            digitalWrite(selectC, BinaryNum[i][2]);
-            delay(1);
-            if(analogRead(readPin1) > lineDetect){
-                line_values[i]++;
-            }
-            progress++;
-        }
-        for(int i = 8; i < 16; i++) {
-            digitalWrite(selectA, BinaryNum[i][0]);
-            digitalWrite(selectB, BinaryNum[i][1]);
-            digitalWrite(selectC, BinaryNum[i][2]);
-            delay(1);
-            if(analogRead(readPin2) > lineDetect){
-                line_values[i]++;
-            }
-            progress++;
-        }
-        for(int i = 16; i < 24; i++){
-            digitalWrite(selectA, BinaryNum[i][0]);
-            digitalWrite(selectB, BinaryNum[i][1]);
-            digitalWrite(selectC, BinaryNum[i][2]);
-            delay(1);
-            if(analogRead(readPin3) > lineDetect){
-                line_values[i]++;
-            }
-                progress++;
-        }
-        bool line_bool =0;
-        for(int i = 0; i < 24; i++){
-            if(line_values[i] >= 2){
-                line_stats[i] = true;
-                line_bool = 1;
-            } else {
-                line_stats[i] = false;
-            }
-            progress++;
-        }
-        if(line_bool == 1){
-            return true;
-        } else {
-            return false;
-        }
-    }
+int LINE::get_azimuth() {
+    line.read();
+    return get_linedeg();
 }
 
-int LINE::get_azimuth() {
+int LINE::read(){ //読み取りを24かいを三回繰り返して当たっていたら配列に１足して　２以上でboolをtrue
+    int line_value [24]={0};
+    progress = 0;
+    for(int i=0; i<3; i++){
+    for(int i = 0; i < 8; i++){//i8
+        digitalWrite(selectA, BinaryNum[i][0]);
+        digitalWrite(selectB, BinaryNum[i][1]);
+        digitalWrite(selectC, BinaryNum[i][2]);
+        delay(1);
+        if(analogRead(readPin1) > lineDetect){
+            line_value[i]++;
+        }
+        progress++;
+    }
+    for(int i = 8; i < 16; i++){
+        digitalWrite(selectA, BinaryNum[i][0]);
+        digitalWrite(selectB, BinaryNum[i][1]);
+        digitalWrite(selectC, BinaryNum[i][2]);
+        delay(1);
+        if(analogRead(readPin2) > lineDetect){
+            line_value[i]++;
+        }
+        progress++;
+    }
+    for(int i = 16; i < 24; i++){
+        digitalWrite(selectA, BinaryNum[i][0]);
+        digitalWrite(selectB, BinaryNum[i][1]);
+        digitalWrite(selectC, BinaryNum[i][2]);
+        delay(1);
+        if(analogRead(readPin3) > lineDetect){
+            line_value[i]++;
+        }
+            progress++;
+    }
+    bool line_bool =0;
+    for(int i = 0; i < 24; i++){
+        if(line_value[i] >= 2){
+            line_status[i] = true;
+            line_bool = 1;
+        }
+        else{
+            line_status[i] = false;
+        }
+        progress++;
+    }
+    if(line_bool == 1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+}
+
+int LINE::get_linedeg() {
     for(int i = 0; i < NUMLines; i++) {//初期化
-        line_stats[i] = false;
+        line_status[i] = false;
         line_detect[i] = 999;
     }
     count =0;
     for(int i = 0; i < NUMLines; i++) {
-        if (line_stats[i] == true) {
-            if(line_stats[i+1] == true) {
+        if (line_status[i] == true) {
+            if(line_status[i+1] == true) {
                 i++;
-                line.adddeg(i,i+1);
+                add_linedeg(i,i+1);
             }
             else {
-                line.adddeg(i,999);
+                add_linedeg(i,999);
             }
         }
     }
@@ -124,35 +122,31 @@ int LINE::get_azimuth() {
     else if(count == 4) {
         return (line_detect[0] + line_detect[1] + line_detect[2] + line_detect[3]) / 4;
     }
-    return ラインの方位角;
 }
 
 void LINE::add_linedeg(int num,int num2) {
-    if(num2 == 999){ //999はエラー用
+    if(num2 == 999){//999はエラー用
         line_detect[count] = Line_deg_list_24[num];
     }
     else{
-        line_detect[count] =  (Line_deg_list_24[num] + Line_deg_list_24[num2]) / 2;
+        line_detect[count] =  (Line_deg_list_24[num]+Line_deg_list_24[num2])/2;
     }
     count++;
 }
 
-int LINE::get_magnitude(int linedeg ,int linedeg2) {
-    int dist = 0;
+void LINE::print_pizel() {//neopixelの表示　　AIへ、「モニターではない！！」
+    mypixel.clear();
+    mypixel.brightness(999);
+    for(int i=0; i<5; i++){
+        if(line_detect[i] == 999){
+            mypixel.closest(line_detect[i], 0, 255, 225);
+        }
+    }
+    mypixel.closest(get_linedeg(), 255, 130, 0);
 }
-int LINE::get_dist(int linedeg ,int linedeg2)
+
+int get_dist(int linedeg ,int linedeg2)
 {
-    int linedist = 0;
-    int theata=general.calculate_deg('s',linedeg2, linedeg);
-    linedist=cos(radians(theata))*sensordist;
-    return linedist;
+    int dist = 0;
+    general.calculate_deg('s',linedeg2, linedeg);
 }
-
-
- for(int i = 0; i < 5; i++){
-  if(line_detect[i] == 999){
-         mypixel.closest(line_detect[i], 0, 255, 225);
-     }
- }
- mypixel.closest(get_linedeg(), 255, 130, 0);
-
